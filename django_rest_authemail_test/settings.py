0b0e736b515d5c0399f2040dc152a8652968095b
jiy12367 <jiy12367@gmail.com>
2021-11-19 16:11:44 +0900

    생성한 계정 관리 어플리케이션 사용을 위한 settings.py 수정

--- django_rest_authemail_test/settings.py
+++ django_rest_authemail_test/settings.py
@@ -41,7 +41,15 @@ INSTALLED_APPS = [
     # 토큰인증을 위한 라이브러리 추가
     'rest_framework',
     'rest_framework.authtoken',
+
+    # authemail 라이브러리 사용을 위한
+    'authemail',
+    # 생성한 계정 관리 어플리케이션 등록
+    'accounts',
 ]
+
+# 디폴트 유저 모델 사용자 설정 유저로 설정
+AUTH_USER_MODEL = 'accounts.MyUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
